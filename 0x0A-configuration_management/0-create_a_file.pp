287b366ed6e469c1b6d4caedccdb8d30391bc2b2
KFredlund <katihomes@gmail.com>
2020-02-08 06:57:02 +0000

    Add path

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # A puppet manifest to create a file
 file { '/tmp/holberton':
   ensure  => 'file',
-  replace => 'yes',
+  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => "I love Puppet\n",
-  type    => 'file',
 }
